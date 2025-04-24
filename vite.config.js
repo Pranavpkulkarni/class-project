b2ce985ce952292f5d0d574e94b5d884689d2687
Pranav <pranavkulkarni1406@gmail.com>
2025-04-24 21:58:03 +0530

    vite issue solve

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/class-project/', // Add this line, replace 'class-project' with the name of your repo
 })
