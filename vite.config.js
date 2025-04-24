023f9fe48d20e0c352ed474650e8b2b071334248
Pranav <pranavkulkarni1406@gmail.com>
2025-04-24 22:13:22 +0530

    test

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: 'class-project/', // Add this line, replace 'class-project' with the name of your repo
+  base: '/class-project/', // Add this line, replace 'class-project' with the name of your repo
 })
